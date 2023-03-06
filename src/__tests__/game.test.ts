d0bfe7b858f7eae6659b951cc982e2c6b3455c35
Scott Abraham <57914239+scott-mysa@users.noreply.github.com>
2023-03-06 17:26:51 +0000

    Co-authored-by: bails <baileyd177@users.noreply.github.com> Co-authored-by: John Hawley <hawler@users.noreply.github.com>

--- src/__tests__/game.test.ts
+++ src/__tests__/game.test.ts
@@ -1,46 +1,83 @@
-import {Game, getBank, setBank, updateBankBalance, startGame, getDay} from '../game'
+import {
+  Game,
+  getBank,
+  setBank,
+  updateBankBalance,
+  startGame,
+  getDay,
+  simulateDay,
+  simulateDayResult,
+  getForecast,
+} from "../game";
 
-describe('game initilization', () => {
-   
-    it('Initialize game has correct starting values', () => {
-        startGame()
-        expect(getBank()).toBe(10)
-        expect(getDay()).toBe(1)
-    })
+describe("game initilization", () => {
+  it("Initialize game has correct starting values", () => {
+    startGame();
+    expect(getBank()).toBe(10);
+    expect(getDay()).toBe(1);
+  });
+});
+describe("game tests", () => {
+  beforeEach(() => {
+    startGame();
+  });
+  it("is defined", () => {
+    expect(Game).toBeDefined();
+  });
+  it("Returns player bank value", () => {
+    const bankResult = getBank();
+    expect(bankResult).toBe(10);
+  });
+  it("Allows setting of bank value", () => {
+    let newBank = getBank() + 1;
+    setBank(newBank);
+    expect(getBank()).toBe(newBank);
+  });
 
-})
-describe('game tests', () => {
-    beforeEach(() => {
-        startGame()
-    })
-    it('is defined', () => {
-        expect(Game).toBeDefined();
-    })
-    it('Returns player bank value', () => {
-        const bankResult = getBank()
-        expect(bankResult).toBe(10) 
-    })
-    it('Allows setting of bank value', () => {
-        let newBank = getBank() + 1
-        setBank(newBank)
-        expect(getBank()).toBe(newBank)
-    })
+  const values = [
+    [10, 5, 15],
+    [10, -5, 5],
+    [10, 0, 10],
+  ];
 
-    const values = [
-        [10,5,15],
-        [10, -5, 5],
-        [10, 0, 10]
-    ]
+  it.each(values)(
+    "Changes the value of bank by a value",
+    (setAmount, updateAmount, expected) => {
+      setBank(setAmount);
+      updateBankBalance(updateAmount);
+      expect(getBank()).toBe(expected);
+    }
+  );
 
-    it.each(values)('Changes the value of bank by a value', (setAmount, updateAmount, expected) => {
-        setBank(setAmount)
-        updateBankBalance(updateAmount)
-        expect(getBank()).toBe(expected)
-    })
+  it("simulateDay advances to the next day", () => {
+    startGame();
+    simulateDay();
+    expect(getDay()).toBe(2);
+    simulateDay();
+    expect(getDay()).toBe(3);
+  });
 
-    it.todo('getDay returns the current day')
+  it("The game ends on day 7", () => {
+    let result: simulateDayResult;
+    startGame();
+    for (let i = 0; i <= 6; i++) {
+      result = simulateDay();
+      expect(result.gameOver).toBe(false);
+    }
+    result = simulateDay();
+    expect(result.gameOver).toBe(true);
+  });
 
-    it.todo('simulateDay advances to the next day')
+  const forecastValues: Array<[1|2|3 , string]> = [
+    [1, "Overcast"],
+    [2, "Clear"],
+    [3, "Sunny"],
+  ];
 
-    it.todo('simulateDay does not advanced past end of game')
-})
+  it.each(forecastValues)(
+    "getForecast method returns correct forecase values",
+    (seed, forecast) => {
+      expect(getForecast(seed)).toBe(forecast)
+    }
+  );
+});
--- src/game.ts
+++ src/game.ts
@@ -5,6 +5,8 @@ export const Game = () => {
 let bank: number;
 let day: number;
 
+const maxDay = 7;
+
 export const startGame = () => {
   bank = 10;
   day = 1;
@@ -24,3 +26,24 @@ export const updateBankBalance = (change: number) => {
 export const getDay = () => {
   return day;
 };
+
+export type simulateDayResult = { gameOver: boolean };
+
+export const simulateDay = (): simulateDayResult => {
+  if (day <= maxDay) {
+    day++;
+    return { gameOver: false };
+  }
+  return { gameOver: true };
+};
+
+enum Forecast {
+  "Overcast" = 1,
+  "Clear",
+  "Sunny"
+}
+
+export const getForecast = (chance: 1 | 2 | 3 ): string => {
+      return Forecast[chance]
+}
+// export const finishGame = (): any => {}
