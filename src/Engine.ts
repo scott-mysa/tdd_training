300eb8ca84af67a34babb6d4c4378e97bff120ee
Scott Abraham <57914239+scott-mysa@users.noreply.github.com>
2023-04-03 16:32:37 +0000

    Co-authored-by: John Hawley <hawler@users.noreply.github.com> Co-authored-by: Nibraj <Nibraj@users.noreply.github.com>

--- src/Engine.ts
+++ src/Engine.ts
@@ -1,8 +1,30 @@
 export class Engine {
-    
-    balance = 10
+  balance = 10;
 
-    getBalance() {
-        return this.balance
+  getBalance() {
+    return this.balance;
+  }
+
+  getMaxCustomer(
+    numCustomer: number,
+    weatherMultiplier: number,
+    priceMultiplier: number
+  ) {
+    const base = numCustomer * weatherMultiplier;
+    return Math.ceil(base * (1 + priceMultiplier));
+  }
+
+  calculatePriceMultiplier(price: number)
+  {
+    let priceMultiplier = 0;
+    if (price === 0.25)
+    {
+        return priceMultiplier;
     }
+    else {
+        const count = (price - 0.25)/0.05;
+        priceMultiplier = -0.025*count;
+    }
+    return Math.round(priceMultiplier*1000)/1000;
+  }
 }
--- src/__tests__/engine.test.ts
+++ src/__tests__/engine.test.ts
@@ -1,18 +1,43 @@
-import {Engine} from "../Engine";
+import { Engine } from "../Engine";
 
 describe("game engine tests", () => {
-  
-  
   it("is defined", () => {
     expect(Engine).toBeDefined();
   });
 
   it("initialize the game starting balance with $10", () => {
     const engine = new Engine();
-    expect (engine.getBalance()).toBe(10);
-  })
+    expect(engine.getBalance()).toBe(10);
+  });
 
-  it("", () => {
-  })
+  const cases = [[25, 1.7, -0.125, 38]];
 
+  it.each(cases)(
+    "expect the number of customers for the given weather",
+    (base, weatherMultiplier, priceMultiplier, expectedCustomer) => {
+      const game = new Engine();
+      const result = game.getMaxCustomer(
+        base,
+        weatherMultiplier,
+        priceMultiplier
+      );
+      expect(result).toEqual(expectedCustomer);
+    }
+  );
+
+  const priceMultiplierCases = [
+    [0.25, 0],
+    [0.2, 0.025],
+    [0.3, -0.025],
+    [0.35, -0.05],
+    [2, -0.875],
+  ];
+
+  it.each(priceMultiplierCases)(
+    "generated price multiplier from cup price",
+    (cupPrice, priceMultiplier) => {
+      const engine = new Engine();
+      expect(engine.calculatePriceMultiplier(cupPrice)).toEqual(priceMultiplier);
+    }
+  );
 });
